bcc9b385974260adef5a150de9ef56093717d863
Shay <shay291@gmail.com>
2020-11-03 19:59:20 +0200

    connect to mongo using mongoose, added user model

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import User from "../models/userModel.js";
+import data from "../data.js";
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+
+const userRoutes = express.Router();
+
+userRoutes.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
+      await User.deleteMany({});
+      const createdUsers = await User.insertMany(data.users);
+      res.send({ createdUsers });
+  })
+);
+
+export default userRoutes;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 import data from './data.js';
 import express from 'express';
+import mongoose from 'mongoose'
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
+mongoose.connect(process.env.MONGOURI || 'mongodb+srv://admin:KcsPiz15r6MHUXsz@cluster0.oezaw.mongodb.net/amazonclone?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
+mongoose.connection.on('connected', ()=> {
+    console.log('connected to mongo');
+})
+
+mongoose.connection.on('error', (err)=> {
+    console.log('error connecting',err);
+})
 
 app.get('/', (req, res) => {
     res.send("Server is ready");
@@ -21,6 +35,14 @@ app.get('/api/products/:id', (req, res) => {
         res.status(404).send({message: 'Product not Found'})
     }
     res.send(data.products);
+});
+
+
+app.use('/api/users', userRoutes);
+
+
+app.use((err,req,res,next)=>{
+    res.status(500).send({message: err.message})
 });
 
 const port = process.env.PORT || 5000
