e69bb9b6c2e827cc59fcad1dd2cbf1b3f3390ee2
Shay <shay291@gmail.com>
2020-11-03 16:37:49 +0200

    redux and thunk added including store, product actions, constants and reducers, for fetching products and product details

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,57 +1,79 @@
 import { Link, useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 import Rating from "../components/Rating";
-import React from "react";
-import data from "../data";
+import { detailsProduct } from "../actions/productActions";
 
 export default function ProductScreen() {
   const params = useParams();
-  const product = data.products.find((x) => x._id === params.id);
-  if (!product) {
-    return <div> Product Not Found</div>;
-  }
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    dispatch(detailsProduct(params.id))
+  },[dispatch,params.id])
+  
   return (
     <div>
-        <Link to="/">Back to results</Link>
-      <div className="row top">
-        <div className="col-2">
-          <img className="large" src={product.image} alt={product.name} />
-        </div>
-        <div className="col-1">
-          <ul>
-            <li>{product.name}</li>
-            <li>
-              <Rating rating={product.rating} numReviews={product.numReviews} />
-            </li>
-            <li>Price: ${product.price}</li>
-            <li>Description: {product.description}</li>
-          </ul>
-        </div>
-        <div className="col-1">
-          <div className="card card-body">
-            <ul>
-              <li>
-                <div className="row">
-                  <div> Price</div>
-                  <div className="price">${product.price}</div>
-                </div>
-              </li>
-              <li>
-                <div className="row">
-                  <div>Status</div>
-                  <div >{product.countInStock>0? <span className="success">In stock</span> :
-                  <span className="danger">Unavailable</span>}</div>
-                </div>
-              </li>
-              <li>
-                <button className="primary block">
-                  Add To Cart
-                </button>
-              </li>
-            </ul>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div>
+          <Link to="/">Back to results</Link>
+          <div className="row top">
+            <div className="col-2">
+              <img className="large" src={product.image} alt={product.name} />
+            </div>
+            <div className="col-1">
+              <ul>
+                <li>{product.name}</li>
+                <li>
+                  <Rating
+                    rating={product.rating}
+                    numReviews={product.numReviews}
+                  />
+                </li>
+                <li>Price: ${product.price}</li>
+                <li>Description: {product.description}</li>
+              </ul>
+            </div>
+            <div className="col-1">
+              <div className="card card-body">
+                <ul>
+                  <li>
+                    <div className="row">
+                      <div> Price</div>
+                      <div className="price">${product.price}</div>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="row">
+                      <div>Status</div>
+                      <div>
+                        {product.countInStock > 0 ? (
+                          <span className="success">In stock</span>
+                        ) : (
+                          <span className="danger">Unavailable</span>
+                        )}
+                      </div>
+                    </div>
+                  </li>
+                  <li>
+                    <button className="primary block">Add To Cart</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
